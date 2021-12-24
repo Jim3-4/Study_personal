0a9b6135cbff9c7463e35c67199eb6d63355c9e7
Jim3-4 <kimjm0304@naver.com>
2021-12-24 09:06:46 +0900

    Create 1.java

---
+++ javastudy/1.java
@@ -0,0 +1,11 @@
+public class Dada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte ba1=125;
+		for (int i=0; i<5; i++) {
+			System.out.println(ba1);
+		}
+	}
+
+}
