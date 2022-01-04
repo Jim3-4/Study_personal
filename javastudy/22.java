23e493037fbb529506c488e58e18584661a5c1f7
Jim3-4 <kimjm0304@naver.com>
2022-01-05 00:51:58 +0900

    1-5

---
+++ javastudy/22.java
@@ -0,0 +1,13 @@
+package smhrd;
+
+public class CarExample extends Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar=new Car();
+		
+		myCar.speed=44;
+		System.out.println(myCar.speed);
+	}
+
+}
---
+++ javastudy/23.java
@@ -0,0 +1,12 @@
+package smhrd;
+
+public class KoreanExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated	 method stub
+
+		Korean k1=new Korean("박자바", "01125-123545");
+		System.out.println(k1.name);
+	}
+
+}
