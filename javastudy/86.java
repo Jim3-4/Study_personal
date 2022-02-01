fbbae8c8ab3ca5826444d78900c09dd0eda83c18
Jim3-4 <kimjm0304@naver.com>
2022-02-02 00:33:52 +0900

    2-2

---
+++ javastudy/86.java
@@ -0,0 +1,11 @@
+package twoInterface;
+
+public class MessageListener implements Button.OnClickListener{
+
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		System.out.println("메세지를 보냅니다.");
+	}
+
+}
