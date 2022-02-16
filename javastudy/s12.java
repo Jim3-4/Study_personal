05e218f6951a53e6797dede49b090742159bddcf
applejam <90122837+Jim3-4@users.noreply.github.com>
2022-02-16 15:48:43 +0900

    Create s12.java

---
+++ javastudy/s12.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class Ex10_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean gender=true;
+		System.out.printf("성별은 %b",gender);
+		
+		
+	}
+
+}
