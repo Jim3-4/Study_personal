cbf5e8cf12d287bef56de71a5cd4bc2933a13316
applejam <90122837+Jim3-4@users.noreply.github.com>
2022-03-11 10:23:30 +0900

    Add files via upload

---
+++ days12/Ex03.java
@@ -0,0 +1,22 @@
+package days12;
+
+/**
+ * @author 지민
+ * @date 2022. 3. 4. - 오전 9:47:41
+ * @subject page189 배열의 길이 변경하기
+ * @content  p189 배열 초기화 
+ * 
+ */
+public class Ex03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] m; //스택에 m 배열공간이 주어진다 
+		new int [3];  // new 연산자에 의해 힙 영역에 메모리 [][][] 할당 
+		//new 연산자를 사용하면 힙 영역에 메모리 할당됨 
+	}
+
+}
