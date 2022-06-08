ee617038cb7223baec02e96d6da95faea362333c
Jim3-4 <kimjm0304@naver.com>
2022-06-08 15:36:53 +0900

    6/8

---
+++ webPro/src/main/webapp/javaScript/days06/ex08_message.js
@@ -0,0 +1,7 @@
+
+const message= () =>{
+    const name='hong';
+    const age=30;
+    return name+'is'+age+'years old';
+}
+export default message;
---
+++ webPro/src/main/webapp/javaScript/days06/ex08_person.js
@@ -0,0 +1,11 @@
+/* 
+export 내보내기 
+Named 
+Default
+두가지유형 
+
+ */
+export const name="admin";
+export const age=20;
+
+export {name, age};
