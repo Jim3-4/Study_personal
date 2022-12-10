b8b3c04cd82e8e79577b2315209a0e316bac79d6
applejam <90122837+Jim3-4@users.noreply.github.com>
2022-12-10 10:58:50 +0900

    Update 다음에올숫자.java

--- 코딩테스트문제/다음에올숫자.java
+++ 코딩테스트문제/다음에올숫자.java
@@ -3,7 +3,7 @@
         if((common[1]-common[0] )==(common[2]-common[1] )) {
         	answer=common[common.length-1]+(common[1]-common[0] );
         }else {
-        	answer=common[common.length-1]+(common[1]/common[0] );
+        	answer=common[common.length-1]*(common[1]/common[0] );
         }
         return answer;
     }
